7a158ced83945265c3d7d3d5814ea8e697e010b9
unknown <ctk5520@C2435-18.main.ad.rit.edu>
2015-05-04 15:42:42 -0400

    -Updated structure

--- project 3 - 3D fps/js/main.js
+++ project 3 - 3D fps/js/main.js
@@ -7,24 +7,45 @@ var scene;
 var camera;
 var renderer;
 
+//Constant Objects
+var GAME = Object.freeze({
+    width: 640,
+    height: 360,
+    fieldWidth: 400,
+    fieldHeight: 200
+});
+var MATERIAL = Object.seal({
+    wallMaterial: undefined,
+    groundMaterial: undefined,
+    bulletMaterial: undefined
+});
+
 function setup(){
 
     // create a scene, that will hold all our elements such as objects, cameras and lights.
     scene = new THREE.Scene();
 
     // create a camera, which defines where we're looking at.
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(45, GAME.width / GAME.height, 0.1, 1000);
 
     // create a render and set the size
     renderer = new THREE.WebGLRenderer();
 
     renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMapEnabled = true;  
+    renderer.shadowMapEnabled = true;
+    
+    createCamera();
+    createMaterials();
+    createGeometry();
+    createModels();
+    createLights();
 }
 
 function createMaterials(){
     
+    MATERIAL.groundMaterial = 
+        new THREE.MeshLambertMaterial({color: 0x4BD121});
 }
 
 function createGeometry(){
@@ -46,7 +67,11 @@ function createLights(){
 function update(){
     
     requestAnimationFrame(update);
+    
+    //draw
 	renderer.render(scene, camera);
+    
+    //update physics
 }
 
 //functions to do things
